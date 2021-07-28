be71334b6f046ff558674f088e1c2804473dc04d
yahyaayabakan <ayabakanyahya@gmail.com>
2021-07-28 18:41:49 +0100

    Created pageSubtitleOptions WebElement en CalendarEventsPage.java class

--- src/test/java/com/vytrack/pages/CalendarEventsPage.java
+++ src/test/java/com/vytrack/pages/CalendarEventsPage.java
@@ -1,6 +1,7 @@
 package com.vytrack.pages;
 
 import com.vytrack.utilities.Driver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -13,5 +14,8 @@ public class CalendarEventsPage extends BasePage {
 
     @FindBy(css = "[title='Create Calendar event']")
     public WebElement createCalendarEvent;
+
+    @FindBy(xpath = "//div[@class='btn btn-link dropdown-toggle']")
+    public WebElement pageSubtitleOptions;
 
 }
