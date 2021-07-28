3b8eb6b11e2743d5e0b180e3605c108c1e00976f
yahyaayabakan <ayabakanyahya@gmail.com>
2021-07-28 18:47:38 +0100

    Verify that page subtitle "Options" is displayed

--- src/test/java/com/vytrack/test/homework/TestCase_1.java
+++ src/test/java/com/vytrack/test/homework/TestCase_1.java
@@ -1,8 +1,10 @@
 package com.vytrack.test.homework;
 
+import com.vytrack.pages.CalendarEventsPage;
 import com.vytrack.pages.DashboardPage;
 import com.vytrack.pages.LoginPage;
 import com.vytrack.test.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestCase_1 extends TestBase {
@@ -12,6 +14,9 @@ public class TestCase_1 extends TestBase {
         loginPage.loginAsAStoreManager();
         DashboardPage dashboardPage = new DashboardPage();
         dashboardPage.navigateToModule("Activities", "Calendar Events");
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        Assert.assertTrue(calendarEventsPage.pageSubtitleOptions.isDisplayed(),
+                "Verify that page subtitle \"Options\" is displayed");
 
 
 
