38f43042cc86a6bc4c8e873b2e8acd5ef1787bdb
yahyaayabakan <ayabakanyahya@gmail.com>
2021-07-28 18:30:59 +0100

    Created an object from LoginPage class and logged in as a store manager

--- src/test/java/com/vytrack/test/homework/TestCase_1.java
+++ src/test/java/com/vytrack/test/homework/TestCase_1.java
@@ -1,6 +1,15 @@
 package com.vytrack.test.homework;
 
+import com.vytrack.pages.LoginPage;
 import com.vytrack.test.TestBase;
+import org.testng.annotations.Test;
 
 public class TestCase_1 extends TestBase {
+    @Test
+    public void testCase1(){
+    LoginPage testCaseObject = new LoginPage();
+    testCaseObject.loginAsAStoreManager();
+
+
+    }
 }
