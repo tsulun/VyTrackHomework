e4bb1bee3d9368eb534dfbca16f200cac0155196
yahyaayabakan <ayabakanyahya@gmail.com>
2021-07-28 18:38:18 +0100

    Navigate to Activities --> Calendar Events

--- src/test/java/com/vytrack/test/homework/TestCase_1.java
+++ src/test/java/com/vytrack/test/homework/TestCase_1.java
@@ -1,14 +1,18 @@
 package com.vytrack.test.homework;
 
+import com.vytrack.pages.DashboardPage;
 import com.vytrack.pages.LoginPage;
 import com.vytrack.test.TestBase;
 import org.testng.annotations.Test;
 
 public class TestCase_1 extends TestBase {
     @Test
-    public void testCase1(){
-    LoginPage testCaseObject = new LoginPage();
-    testCaseObject.loginAsAStoreManager();
+    public void testCase_1(){
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAsAStoreManager();
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+
 
 
     }
