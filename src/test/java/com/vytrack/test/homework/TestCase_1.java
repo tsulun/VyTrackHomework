81869d59727788af7f5a12817b00ba833d285e26
yahyaayabakan <ayabakanyahya@gmail.com>
2021-07-28 18:56:31 +0100

    Added test name and other test case report requirements

--- src/test/java/com/vytrack/test/homework/TestCase_1.java
+++ src/test/java/com/vytrack/test/homework/TestCase_1.java
@@ -10,15 +10,21 @@ import org.testng.annotations.Test;
 public class TestCase_1 extends TestBase {
     @Test
     public void testCase_1(){
+        extentLogger = report.createTest("page subtitle \"Options\" TEST");
+
+        extentLogger.info("Login as a store manager");
         LoginPage loginPage = new LoginPage();
         loginPage.loginAsAStoreManager();
+
+        extentLogger.info("Navigate to “Activities -> Calendar Events”");
         DashboardPage dashboardPage = new DashboardPage();
         dashboardPage.navigateToModule("Activities", "Calendar Events");
+
+        extentLogger.info("Verify that page subtitle \"Options\" is displayed");
         CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
         Assert.assertTrue(calendarEventsPage.pageSubtitleOptions.isDisplayed(),
                 "Verify that page subtitle \"Options\" is displayed");
 
-
-
+        extentLogger.pass("page subtitle \"Options\" test has PASSED");
     }
 }
