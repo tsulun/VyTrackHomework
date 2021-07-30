9982071d726d1bfe34cee478544be44bea6a0f71
LauraIstoc <lla_ura@yahoo.com>
2021-07-30 12:48:30 +0100

    Checking number of rows and records

--- src/test/java/com/vytrack/test/homework/TestCase_4.java
+++ src/test/java/com/vytrack/test/homework/TestCase_4.java
@@ -3,9 +3,15 @@ package com.vytrack.test.homework;
 import com.vytrack.pages.CalendarEventsPage;
 import com.vytrack.pages.DashboardPage;
 import com.vytrack.pages.LoginPage;
+import com.vytrack.test.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestCase_4 {
+import java.util.List;
+
+public class TestCase_4 extends TestBase {
 
     @Test
     public void nrOfRowsAndRecords(){
@@ -16,12 +22,18 @@ public class TestCase_4 {
         DashboardPage dashboardPage = new DashboardPage();
         dashboardPage.navigateToModule("Activities","Calendar Events");
 
+
         CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        System.out.println(calendarEventsPage.rowsWithoutHeader.size());
 
+        String expectedRecordsNum = "Total Of 1267 Records";
 
+        Assert.assertEquals(calendarEventsPage.rowsWithoutHeader.size(),expectedRecordsNum,"verify the numbers of Rows and Records");
 
 
     }
+
+
 
 
 }
