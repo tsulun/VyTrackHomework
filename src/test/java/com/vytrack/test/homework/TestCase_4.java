7368896abe07af436c533bc3048402789fd6cd58
LauraIstoc <lla_ura@yahoo.com>
2021-07-30 11:50:02 +0100

    Object LoginPage and navigated to the module Calendar Events

--- src/test/java/com/vytrack/test/homework/TestCase_4.java
+++ src/test/java/com/vytrack/test/homework/TestCase_4.java
@@ -1,4 +1,27 @@
 package com.vytrack.test.homework;
 
+import com.vytrack.pages.CalendarEventsPage;
+import com.vytrack.pages.DashboardPage;
+import com.vytrack.pages.LoginPage;
+import org.testng.annotations.Test;
+
 public class TestCase_4 {
+
+    @Test
+    public void nrOfRowsAndRecords(){
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAsAStoreManager();
+
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.navigateToModule("Activities","Calendar Events");
+
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+
+
+
+
+    }
+
+
 }
