58bf7e1eca90f898f4168b9071a0aebf6b22c225
SedaYurdakul <Seda.ztbk@gmail.com>
2021-07-31 19:46:46 +0100

    TC2 is created and logged in as a store manager

--- src/test/java/com/vytrack/test/homework/TestCase_2.java
+++ src/test/java/com/vytrack/test/homework/TestCase_2.java
@@ -1,4 +1,18 @@
 package com.vytrack.test.homework;
 
-public class TestCase_2 {
+import com.vytrack.pages.LoginPage;
+import com.vytrack.test.TestBase;
+import org.testng.annotations.Test;
+
+public class TestCase_2 extends TestBase {
+
+    @Test
+    public void verifyPageNumber() {
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAsAStoreManager();
+        
+
+
+    }
 }
