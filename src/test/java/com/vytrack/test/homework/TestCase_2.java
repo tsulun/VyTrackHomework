567e4e9ae7be6102923a0367699bcf634a767c6e
SedaYurdakul <Seda.ztbk@gmail.com>
2021-07-31 20:30:48 +0100

    Report info added to TC2

--- src/test/java/com/vytrack/test/homework/TestCase_2.java
+++ src/test/java/com/vytrack/test/homework/TestCase_2.java
@@ -4,7 +4,6 @@ import com.vytrack.pages.CalendarEventsPage;
 import com.vytrack.pages.DashboardPage;
 import com.vytrack.pages.LoginPage;
 import com.vytrack.test.TestBase;
-import com.vytrack.utilities.BrowserUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,10 +12,15 @@ public class TestCase_2 extends TestBase {
     @Test
     public void verifyPageNumber() {
 
+        extentLogger = report.createTest("Verify Page Number");
         LoginPage loginPage = new LoginPage();
+
+        extentLogger.info("Logged in as a store manager");
         loginPage.loginAsAStoreManager();
 
         DashboardPage dashboardPage = new DashboardPage();
+
+        extentLogger.info("Navigated to Activities --> Calendar Events");
         dashboardPage.navigateToModule("Activities","Calendar Events");
 
         CalendarEventsPage calendarEventsPage=new CalendarEventsPage();
@@ -24,7 +28,9 @@ public class TestCase_2 extends TestBase {
         String expectedPageNumber="1";
         String actualPageNumber = calendarEventsPage.getPageNumber();
 
+        extentLogger.info("Compared actual and expected page number");
         Assert.assertEquals(actualPageNumber,expectedPageNumber,"Verify page number");
 
+        extentLogger.pass("PASSED");
     }
 }
