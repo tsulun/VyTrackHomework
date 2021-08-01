2a343c95ec07ef0422b1b4b15ac7c70a6883f10e
TeodoruMarin <teodoru652@gmail.com>
2021-08-01 13:06:46 +0100

    Work finished

--- src/test/java/com/vytrack/test/homework/TestCase_3.java
+++ src/test/java/com/vytrack/test/homework/TestCase_3.java
@@ -1,4 +1,43 @@
 package com.vytrack.test.homework;
 
-public class TestCase_3 {
+import com.vytrack.pages.BasePage;
+import com.vytrack.pages.DashboardPage;
+import com.vytrack.pages.LoginPage;
+import com.vytrack.test.TestBase;
+import com.vytrack.utilities.ConfigurationReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCase_3 extends TestBase {
+
+@Test
+    public void loginAsStoreManager(){
+
+    LoginPage loginPage = new LoginPage();
+    loginPage.loginAsAStoreManager();
+
+    DashboardPage dashboardPage = new DashboardPage();
+
+    dashboardPage.waitUntilLoaderScreenDisappear();
+
+    dashboardPage.navigateToModule("Activities","Calendar Events");
+
+    DashboardPage dash = new DashboardPage();
+
+    String actual = dash.getPageNum();
+    String expected = "25";
+
+    Assert.assertEquals(actual,expected,"verify page number is 25");
+
+
+
+
+
+
+
+}
+
+
+
+
 }
